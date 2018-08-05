ed708ceca66c6b786dd96568d8af6c3a498459bd
elwirak4 <kobusevich@gmail.com>
2018-08-05 21:36:29 +0200

    update after 3rd part

--- game.js
+++ game.js
@@ -1,15 +1,10 @@
-var colors = [
-	"rgb(255, 0, 0)",
-	"rgb(255, 255, 0)",
-	"rgb(0, 255, 0)",
-	"rgb(0, 255, 255)",
-	"rgb(0, 0, 255)",
-	"rgb(255, 0, 255)",
-]
+var colors = generateRandomColors(6);
+
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
 var colorDisplay = document.getElementById('colorDisplay');
 var messageDisplay = document.querySelector("#message");
+var  h1 = document.querySelector("h1");
 
 colorDisplay.textContent = pickedColor;
 
@@ -22,9 +17,11 @@ for(var i = 0; i < squares.length; i++){
 		//grab color of clicked square
 		var clickedColor = this.style.backgroundColor;
 		//compare color to pickedColor
+
 		if (clickedColor === pickedColor) {
 			messageDisplay.textContent = "Correct!";
 			changeColors(clickedColor);
+			h1.style.backgroundColor = clickedColor;
 		}else{
 			this.style.backgroundColor = "#232323";
 			messageDisplay.textContent = "Try again";
@@ -44,4 +41,25 @@ function changeColors(color){
 function pickColor(){
 	var random = Math.floor(Math.random() * colors.length);
 	return colors[random];
+}
+
+function generateRandomColors(num){
+	//make an array
+	var arr = []
+	//add num random colors to array
+	for(var i = 0; i <num; i++){
+		//get random color and push into arr
+		arr.push(randomColor());
+	}
+	return arr;
+}
+
+function randomColor(){
+	//pick a "red" from 0 - 255
+	var r = Math.floor(Math.random() * 256);
+	//pick a "green" from 0 - 255
+	var g = Math.floor(Math.random() * 256);
+	//pick a "blue" from 0 - 255
+	var b = Math.floor(Math.random() * 256);
+	return "rgb(" + r  + ", " + g + ", " + b + ")";
 }
